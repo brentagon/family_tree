680c44414c3000c8d668acfea7cd9053315e4817
Brent Bailey & Andrew David Burt <student@epicodus.com>
2015-05-14 14:07:38 -0700

    passes child, parent, sibling specs

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person < ActiveRecord::Base
-    has_many :parents_children, :foreign_key => :child_id, class_name: "Family"
+    has_many :parents_children, :foreign_key => :parent_id, class_name: "Family"
     has_many :parents_kids, through: :parents_children, source: :child
 #    has_many :parent_ids, through: :family
 #    has_many :child_ids, through: :family
 
-    has_many :childrens_parents, :foreign_key => :parent_id, class_name: "Family"
+    has_many :childrens_parents, :foreign_key => :child_id, class_name: "Family"
     has_many :kids_parents, through: :childrens_parents, source: :parent
 
     def has_children?
@@ -16,18 +16,43 @@ class Person < ActiveRecord::Base
     end
 
     def add_parent(name)
-        child = self
-        parent = Person.create({:name => name})
+        #child = self
+        parent = self.kids_parents.create(:name => name)
     #    child.parents_children.create(:child_id => child.id, :parent_id => parent.id)
     #    parent.childrens_parents.update(:child_id => child.id, :parent_id => parent.id)
     #    child.update(parent_id: parent.id)
 
     #    # self.kids_parents.create({:name => name})
     #    # parent.parents_kids.where(child_id: nil).first.update(child_id: child.id)
-        # parent.parents_children.create(:child_id => child.id, :parent_id => parent.id)
+        # family = Family.where(parent_id: parent.id, child_id: nil)
+        # family.first.update(child_id: child.id)
+        # parent.parents_kids.push(child)
+
         # Family.where(parent_id: parent.id, child_id: nil).first.update(child_id: child.id)
         # parent_id = Family.where(parent_id: parent.id, child_id: child.id).first.parent_id
         # parent_1 = Person.find(parent_id)
+    #    binding.pry
         parent
     end
+
+    def siblings
+        all_relations = Family.all
+        parent = 0
+        all_relations.each do |relation|
+            if relation.child_id == self.id
+                parent = relation.parent_id
+            end
+        end
+        return_array = []
+        all_relations.each do |relation|
+            if relation.parent_id == parent
+                if relation.child_id != self.id
+                    return_array.push(relation.child_id)
+                end
+            end
+        end
+        return_array
+
+    end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,12 +8,30 @@ describe(Person) do
     #    binding.pry
         expect(parent_1.has_children?)
     end
+
     it('will check for existence of parent(s)') do
         child_1 = Person.create(:name => 'Susie')
         parent_1 = child_1.add_parent('Frank')
-binding.pry
         expect(child_1.has_parents?)
         expect(parent_1.has_children?)
-       binding.pry
+        younger_child = Person.create(:name => "lil Sue")
+        younger_child.kids_parents.push(child_1)
+        expect(younger_child.kids_parents).to eq([child_1])
+        expect(child_1.kids_parents).to eq([parent_1])
+        expect(child_1.parents_kids).to eq([younger_child])
+    #    binding.pry
     end
+
+    it("tells you its siblings") do
+        test_sibling_1 = Person.create(:name => "Jimmy")
+        test_sibling_2 = Person.create(:name => "Susie")
+        test_sibling_3 = Person.create(:name => "Billy")
+        test_parent = Person.create(:name => "Guillaume")
+        test_sibling_1.kids_parents.push(test_parent)
+        test_sibling_2.kids_parents.push(test_parent)
+        test_sibling_3.kids_parents.push(test_parent)
+    #    binding.pry
+        expect(test_sibling_1.siblings).to eq([test_sibling_2.id, test_sibling_3.id])
+    end
+
 end
